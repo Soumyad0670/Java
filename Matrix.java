a0146b0288dde98e828ae11e50ce7e9ebe13c405
Soumyad0670 <soumyadeep0989@gmail.com>
2025-12-04 13:20:12 +0530

    commit

--- Matrix.java
+++ Matrix.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
 public class Matrix {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int i,j;
-        int m[][]=new int[4][4];
-        int n[][]=new int[4][4];
-        System.out.println("Enter the elements of the matrix");
-        for(i=0;i<4;i++){
-            for(j=0;j<4;j++)
-            m[i][j]=sc.nextInt();
-        }
-        System.out.println("The matrix");
-        for(i=0;i<4;i++){
-            for(j=0;j<4;j++)
-            System.out.print(m[i][j]+" ");
-            System.out.println();
-        }
-        for(i=0;i<4;i++){
-            for(j=0;j<4;j++)
-            n[j][i]=m[i][j];
-        }        
-        System.out.println("The transpose of the matrix");
-        for(j=0;j<4;j++){
-            for(i=0;i<4;i++)
-            System.out.print(n[j][i]+" ");
-            System.out.println();
+        try (Scanner sc = new Scanner(System.in)) {
+            int i, j;
+            int m[][] = new int[4][4];
+            int n[][] = new int[4][4];
+            System.out.println("Enter the elements of the matrix");
+            for (i = 0; i < 4; i++) {
+                for (j = 0; j < 4; j++) {
+                    m[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println("The matrix");
+            for (i = 0; i < 4; i++) {
+                for (j = 0; j < 4; j++) {
+                    System.out.print(m[i][j] + " ");
+                }
+                System.out.println();
+            }
+            for (i = 0; i < 4; i++) {
+                for (j = 0; j < 4; j++) {
+                    n[j][i] = m[i][j];
+                }
+            }
+            System.out.println("The transpose of the matrix");
+            for (j = 0; j < 4; j++) {
+                for (i = 0; i < 4; i++) {
+                    System.out.print(n[j][i] + " ");
+                }
+                System.out.println();
+            }
         }
     }
 }
